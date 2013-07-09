d0de60c3a97bbe33579a2e26dd6252c04b45c440
Michael A Tomcal <mtomcal@d131-201.uoregon.edu>
2013-07-09 15:32:34 -0700

    Fixed dist again

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
   };
 
   try {
-    yeomanConfig.app = require('./component.json').appPath || yeomanConfig.app;
+    yeomanConfig.app = require('./bower.json').appPath || yeomanConfig.app;
   } catch (e) {}
 
   grunt.initConfig({
@@ -293,7 +293,7 @@ module.exports = function (grunt) {
     'concat',
     'copy',
     'cdnify',
-    //'ngmin',
+    'ngmin',
     'uglify',
     'rev',
     'usemin'
