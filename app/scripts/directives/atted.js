f9fd905463e0f6d87278f668d36ac8d1928d6d43
Michael A Tomcal <mtomcal@d131-201.uoregon.edu>
2013-11-19 14:01:01 -0800

    Fixed ATTED

--- app/scripts/directives/atted.js
+++ app/scripts/directives/atted.js
@@ -5,7 +5,7 @@ angular.module('pogsUiApp').
       scope: {
         locus: '=',
       },
-      template: '<a class="btn btn-warning" tooltip-placement="bottom" tooltip="Click to learn about the gene coexpression network for the Arabidopsis ortholog" target="_blank" href="http://atted.jp/cgi-bin/locus.cgi?loc={{locus}}">ATTED-II Coexpression Database</a>',
+      template: '<a class="btn btn-warning btn-mini" tooltip-placement="bottom" tooltip="Click to learn about the gene coexpression network for this Arabidopsis ortholog" target="_blank" href="http://atted.jp/cgi-bin/locus.cgi?loc={{locus}}">ATTED-II</a>',
       link: function (scope, element, attr) {
         scope.$watch('locus', function () {
           if (!scope.locus || !scope.locus.match(/()(AT[\w|\d]+)/)) {
